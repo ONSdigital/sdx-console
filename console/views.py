db876e33a2c6315817e5fe02d094f92142484bf8
Daniel Bridson <briddo@gmail.com>
2016-12-21 11:51:13 +0000

    debug

--- console/views.py
+++ console/views.py
@@ -330,8 +330,34 @@ def validate():
 
 @app.route('/ftp.json')
 def ftp_list():
-    return jsonify(get_ftp_contents())
-
+    # return jsonify(get_ftp_contents())
+    return """
+{
+  "image": [
+    {
+      "filename": "S000000003.JPG"
+    },
+    {
+      "filename": "S000000004.JPG"
+    }
+  ],
+  "index": [
+    {
+      "filename": "EDC_023_20161213_1002.csv"
+    }
+  ],
+  "pck": [
+    {
+      "filename": "023_1002"
+    }
+  ],
+  "receipt": [
+    {
+      "filename": "REC1312_1002.DAT"
+    }
+  ]
+}
+    """
 
 @app.route('/view/<datatype>/<filename>')
 def view_file(datatype, filename):
