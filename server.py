e61c86e5e7b8103caf92bc3b253767310b841a34
James Gardiner <james@jgardiner.co.uk>
2017-03-10 11:55:09 +0000

    Log version number on startup

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from console import app
+import logging
 import os
 
+from console import __version__
+
+logger = logging.getLogger(__name__)
+
 if __name__ == '__main__':
+    logger.info("Current version: {}".format(__version__))
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
