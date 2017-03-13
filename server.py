60a008553420bdd67e7d568be5e8c4cf560c18a6
James Gardiner <james@jgardiner.co.uk>
2017-03-13 12:52:12 +0000

    Update event logging format

--- server.py
+++ server.py
@@ -7,6 +7,6 @@ from console import __version__
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    logger.info("Current version: {}".format(__version__))
+    logger.info("Starting server: version='{}'".format(__version__))
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
