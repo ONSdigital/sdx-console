83453c564ff0cfb57e24cb903ce7f4c329d58ace
Andrew Millar <andrew.millar@ons.gov.uk>
2017-04-20 15:36:40 +0100

    adding working heartbeat

--- console/views.py
+++ console/views.py
@@ -13,3 +13,12 @@ import time
 
 logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
 logger = wrap_logger(logging.getLogger(__name__))
+
+def heartbeat():
+    heartbeats = 0
+    while True:
+        logger.info("Heartbeat " + str(heartbeats))
+        heartbeats += 1
+        time.sleep(30.0)
+
+heartbeat()
