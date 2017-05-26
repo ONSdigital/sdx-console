1990aefd233cdf4df4d9b4aea189cec3f39526c7
Andrew Millar <andrew.millar@ons.gov.uk>
2017-05-26 13:01:23 +0100

    can now return all entries in store, inclues temporary endpoint to add data to db

--- console/views.py
+++ console/views.py
@@ -5,8 +5,10 @@ import json
 from flask import render_template
 from flask import request
 import flask_security
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from structlog import wrap_logger
 
+from console.authentication import db
 from console import app
 from console import settings
 from console.helpers.exceptions import ClientError, ServiceError
@@ -23,7 +25,7 @@ def home():
 
 def send_data(url, data=None, request_type="POST"):
     try:
-        if request_type=="POST":
+        if request_type == "POST":
             logger.info("POSTING DATA")
             r = requests.post(url, data)
         else:
@@ -72,38 +74,105 @@ def decrypt():
         return render_template('decrypt.html')
 
 
+class SurveyResponse(db.Model):
+    __tablename__ = 'responses'
+    tx_id = db.Column("tx_id",
+                      UUID,
+                      primary_key=True)
+
+    ts = db.Column("ts",
+                   db.TIMESTAMP(timezone=True),
+                   server_default=db.func.now(),
+                   onupdate=db.func.now())
+
+    invalid = db.Column("invalid",
+                        db.Boolean,
+                        default=False)
+
+    data = db.Column("data", JSONB)
+
+    def __init__(self, tx_id, invalid, data):
+        self.tx_id = tx_id
+        self.invalid = invalid
+        self.data = data
+
+    def __repr__(self):
+        return '<SurveyResponse {}>'.format(self.tx_id)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+def get_store_responses():
+    responses = SurveyResponse.query.all()
+
+    store_data = []
+    for response in responses:
+        store_data.append(response.data)
+
+    return store_data
+
+
 @app.route('/store', methods=['GET'])
 def store():
+    store_data = get_store_responses()
 
+    return render_template('store.html', data=store_data)
+
+
+@app.route('/storetest', methods=['GET'])
+def storetest():
     test_data = json.dumps(
         {
-        "collection": {
-            "exercise_sid": "hfjdskf",
-            "instrument_id": "ce2016",
-            "period": "0616"
-        },
-        "data": {
-            "1": "2",
-            "2": "4",
-            "3": "2",
-            "4": "Y"
-        },
-        "metadata": {
-            "ru_ref": "12345678901A",
-            "user_id": "789473423"
-        },
-        "origin": "uk.gov.ons.edc.eq",
-        "submitted_at": "2017-04-27T14:23:13+00:00",
-        "survey_id": "0",
-        "tx_id": "f088d89d-a367-876e-f29f-ae8f1a26191b",
-        "type": "uk.gov.ons.edc.eq:surveyresponse",
-        "version": "0.0.1"
+            "collection": {
+                "exercise_sid": "hfjdskf",
+                "instrument_id": "ce2016",
+                "period": "0616"
+            },
+            "data": {
+                "1": "2",
+                "2": "4",
+                "3": "2",
+                "4": "Y"
+            },
+            "metadata": {
+                "ru_ref": "12345678901a",
+                "user_id": "789473423"
+            },
+            "origin": "uk.gov.ons.edc.eq",
+            "submitted_at": "2017-04-27T14:23:13+00:00",
+            "survey_id": "0",
+            "tx_id": "f088d89d-a367-876e-f29f-ae8f1a26191d",
+            "type": "uk.gov.ons.edc.eq:surveyresponse",
+            "version": "0.0.1"
+        })
+
+    test_data2 = json.dumps(
+        {
+            "collection": {
+                "exercise_sid": "hfjdskf",
+                "instrument_id": "ce2016",
+                "period": "0616"
+            },
+            "data": {
+                "1": "2",
+                "2": "4",
+                "3": "2",
+                "4": "Y"
+            },
+            "metadata": {
+                "ru_ref": "12345678901a",
+                "user_id": "789473423"
+            },
+            "origin": "uk.gov.ons.edc.eq",
+            "submitted_at": "2017-04-27T14:23:13+00:00",
+            "survey_id": "0",
+            "tx_id": "f088d89d-a367-876e-f29f-ae8f1a26191e",
+            "type": "uk.gov.ons.edc.eq:surveyresponse",
+            "version": "0.0.1"
         })
 
     send_data(settings.SDX_STORE_URL + "responses", data=test_data, request_type="POST")
+    send_data(settings.SDX_STORE_URL + "responses", data=test_data2, request_type="POST")
 
-    response = send_data(settings.SDX_STORE_URL + "responses", request_type="GET")
-    # content = response.content.decode('UTF8')
-    # store_data = json.loads(content)
-
-    return render_template('store.html', data=response.data)
+    return "data sent"
