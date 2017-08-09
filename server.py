765446a8b115e9c18d152317ff2e37c560add023
Ian Edward <ian.edward@ons.gov.uk>
2017-08-09 10:28:39 +0100

    flake8 failures fix

--- server.py
+++ server.py
@@ -1,11 +1,8 @@
 import logging
-from flask import jsonify
-
 from structlog import wrap_logger
 from console import __version__
 from console import app
 from console import settings
-
 
 logger = wrap_logger(logging.getLogger(__name__))
 
@@ -13,4 +10,3 @@ if __name__ == '__main__':
     logger.info("Starting server: version='{}'".format(__version__))
     port = int(settings.PORT)
     app.run(debug=True, host='0.0.0.0', port=port)
-
