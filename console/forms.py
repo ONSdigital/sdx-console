15379d3a6877a6ac327240a0ef85ce4a037bd6f0
Tejas Patel <tejas.patel@ons.gov.uk>
2017-09-05 08:23:12 +0100

    Add password minimum length

--- console/forms.py
+++ console/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class NewUserForm(Form):
     email = StringField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8)])
