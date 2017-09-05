911db2d9393d5958de038a54e495e0ad67eb9bd4
Tejas Patel <tejas.patel@ons.gov.uk>
2017-09-05 11:41:37 +0100

    Add password requirement checks

--- console/forms.py
+++ console/forms.py
@@ -1,8 +1,28 @@
+import re
+
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class NewUserForm(Form):
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired(), Length(min=8)])
+
+    @staticmethod
+    def validate_password(form, field):
+        data = field.data
+
+        if not re.findall('.*[a-z].*', data):
+            msg = '{} should have atleast one lowercase character'.format(field.name)
+            raise ValidationError(msg)
+
+        if not re.findall('.*[A-Z].*', data):
+            msg = '{} should have atleast one uppercase character'.format(field.name)
+            raise ValidationError(msg)
+
+        if not re.findall('.*[0-9].*', data):
+            msg = '{} should have atleast one number'.format(field.name)
+            raise ValidationError(msg)
+
+
