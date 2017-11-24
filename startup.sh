b21ebe635a65f6b60e76cae02063d194a246240c
Adam Mann <manny160389@hotmail.com>
2017-11-24 11:13:13 +0000

    Add https support for prod and preprod

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -z ${PORT+x} ]; 
-then 
-    export PORT=5000; 
+if [ -z ${PORT+x} ];
+then
+    export PORT=5000;
 fi
 
 if [ "$SDX_DEV_MODE" = true ]
 then
     python3 server.py
 else
-    gunicorn -b 0.0.0.0:$PORT server:app
+    gunicorn --certfile $SDX_CONSOLE_SSL_CERTIFICATE --keyfile $SDX_CONSOLE_SSL_KEY -b 0.0.0.0:$PORT server:app
 fi
