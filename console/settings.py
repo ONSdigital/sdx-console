ee2490ace0e6828d9e7882569f4c486c9d28b7ab
dcdarrell9 <darrells_king@hotmail.com>
2017-11-28 13:05:16 +0000

    fix merge conflicts

--- console/settings.py
+++ console/settings.py
@@ -1,5 +1,5 @@
+import json
 import logging
-<<<<<<< HEAD
 import os
 
 from structlog import wrap_logger
@@ -10,12 +10,7 @@ LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-console: %(message
 
 logger = wrap_logger(logging.getLogger(__name__))
 DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', False)
-=======
-import json
 
-LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
-LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-console: %(message)s"
->>>>>>> origin
 
 DB_HOST = os.getenv('POSTGRES_HOST', '0.0.0.0')
 DB_PORT = os.getenv('POSTGRES_PORT', '5432')
@@ -39,18 +34,9 @@ WTF_CSRF_ENABLED = False
 
 PORT = os.getenv("PORT", 5000)
 
-<<<<<<< HEAD
 ENABLE_EMPTY_FTP = os.getenv('ENABLE_EMPTY_FTP', 0)
 
 HB_INTERVAL = os.getenv("HB_INTERVAL", 30)
-=======
-# EQ's jwt_test_keys
-EQ_PUBLIC_KEY = get_key(os.getenv('EQ_PUBLIC_KEY', "jwt_test_keys/sdc-submission-signing-sr-public-key.pem"))
-EQ_PRIVATE_KEY = get_key(os.getenv('EQ_PRIVATE_KEY', "jwt_test_keys/sdc-submission-signing-sr-private-key.pem"))
-
-# Posies jwt_test_keys
-PRIVATE_KEY = get_key(os.getenv('PRIVATE_KEY', "jwt_test_keys/sdc-submission-encryption-sdx-private-key.pem"))
->>>>>>> origin
 
 SDX_STORE_URL = os.getenv("SDX_STORE_URL", "http://sdx-store:5000/responses")
 SDX_VALIDATE_URL = os.getenv("SDX_VALIDATE_URL", "http://sdx-validate:5000/validate")
