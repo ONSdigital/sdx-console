f6dcbd2fdc73b8689390e6e558b67c536ddf3d28
dcdarrell9 <darrells_king@hotmail.com>
2017-12-05 11:11:05 +0000

    add edc_qjson path

--- console/console_ftp.py
+++ console/console_ftp.py
@@ -16,7 +16,7 @@ PATHS = {
     "image": settings.SDX_FTP_IMAGE_PATH + "/Images",
     "index": settings.SDX_FTP_IMAGE_PATH + "/Index",
     "receipt": settings.SDX_FTP_RECEIPT_PATH,
-    "json": settings.SDX_FTP_DATA_PATH
+    "json": settings.SDX_RESPONSE_JSON_PATH
 }
 
 
